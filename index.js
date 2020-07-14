fc98c4f2195831fad0f82f2ab2852d182135bfa5
agusTrih <ant3dong@gmail.com>
2020-07-14 18:29:22 +0700

    add database and ejs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const ejs = require("ejs");
 
 app.use(express.json());
 app.use(
@@ -16,15 +17,15 @@ app.get("/", (req, res) => {
     res.send("Welcome to sequelize & express demo");
 });
 
-// memanggil api
-router.get("/", async (req, res) => {
-    const obj = await fetchData();
-    res.render("./views/pages/home", { obj });
-});
+// // memanggil api
+// router.get("/", async (req, res) => {
+//     const obj = await fetchData();
+//     res.render(", { obj });
+// });
 
-function fetchData() {
-    return fetch("http://localhost:5000/api/movies").then((res) => res.json());
-}
+// function fetchData() {
+//     return fetch("http://localhost:5000/api/movies").then((res) => res.json());
+// }
 
 app.use("/api/movies", require("./routes/movies"));
 
